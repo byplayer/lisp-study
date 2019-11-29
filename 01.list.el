1435e36ca4694c916311ee6c817923b392304dad
byplayer <byplayer100@gmail.com>
2019-11-29 16:49:54 +0800

    :+1: Add basic list function

--- 01.list.el
+++
@@ -1,1 +0,0 @@
-'(rose violet daisy buttercup)
---
+++ 20191129_list.el
@@ -0,0 +1,5 @@
+'(rose violet daisy buttercup)
+
+(car '(rose violet daisy buttercup))
+(cdr '(rose violet daisy buttercup))
+(cons 'orange '(rose violet daisy buttercup))
